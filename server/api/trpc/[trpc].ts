18bff2ad5a67a8184a9e35ea2709ea23d474fbc8
bino <bonoself@gmail.com>
2025-02-21 21:44:05 +0900

    .

--- server/api/trpc/[trpc].ts
+++ server/api/trpc/[trpc].ts
@@ -1,6 +1,6 @@
 import { createNuxtApiHandler } from 'trpc-nuxt';
-import { router } from '@app/server/providers/trpc/trpc';
-import * as routers from '@app/server/providers/trpc/routers';
+import { router } from '@app/server/trpc/trpc';
+import * as routers from '@app/server/trpc/routers';
 
 export const appRouter = router(routers);
 export type AppRouter = typeof appRouter;
