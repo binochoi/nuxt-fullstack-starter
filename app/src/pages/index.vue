0d028e02747e2df7015a9588fff26f12aaa71c1b
bino <bonoself@gmail.com>
2025-06-08 20:05:02 +0900

    .

--- app/src/pages/index.vue
+++ app/src/pages/index.vue
@@ -2,7 +2,6 @@
 const { api } = useRPC();
 const { data } = useAsyncData(async () => await api.health.$get());
 const auth = useAuthStore();
-console.log('sival')
 </script>
 <template>
   <div class="flex flex-col items-center justify-center h-screen gap-4">
