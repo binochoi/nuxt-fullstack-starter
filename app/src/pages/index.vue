643796eee7e2fb20c41ff8d733055ce734e07e33
bino <bonoself@gmail.com>
2025-06-08 16:15:53 +0900

    .

--- app/src/pages/index.vue
+++ app/src/pages/index.vue
@@ -5,10 +5,25 @@ const auth = useAuthStore();
 </script>
 <template>
   <div class="flex flex-col items-center justify-center h-screen gap-4">
-    {{ auth.fetchStatus }}
-    {{ data }}
-    <PrimeButton @click="$router.push('/login')">
-      로그인하기
-    </PrimeButton>
+    <div>
+      auth status: {{ auth.fetchStatus }}
+    </div>
+    <div>
+      health status: {{ data }}
+    </div>
+    <div v-if="auth.fetchStatus === 'fetching'">
+      auth loading...
+    </div>
+    <div v-else-if="auth.fetchStatus === 'error'">
+      auth error !
+    </div>
+    <div v-else>
+      <div v-if="auth.user">
+        {{ auth.user.name }} 로 로그인
+      </div>
+      <PrimeButton @click="$router.push('/login')">
+        로그인하기
+      </PrimeButton>
+    </div>
   </div>
 </template>
