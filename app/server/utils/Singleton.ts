7d298f6bcacd32e4d3c00db80d43c6c1e1677420
bino <bonoself@gmail.com>
2025-07-09 22:10:25 +0900

    remove deprecated Singleton

--- app/server/utils/Singleton.ts
+++
@@ -1,9 +0,0 @@
-export const Singleton = <T>(factory: () => T) => {
-    let instance: T | null = null;
-    return () => {
-        if (!instance) {
-            instance = factory();
-        }
-        return instance;
-    };
-};
