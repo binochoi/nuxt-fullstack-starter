2a9f849af49c70f29f223932ebc3d59517b6925c
bino <bonoself@gmail.com>
2025-07-14 02:00:29 +0900

    feat(ModalContainer): ste content-class

--- app/src/components/ui/ModalContainer.vue
+++ app/src/components/ui/ModalContainer.vue
@@ -5,7 +5,8 @@ import { VueFinalModal } from 'vue-final-modal';
   <VueFinalModal v-bind="{
     overlayTransition: 'vfm-fade',
     contentTransition: 'vfm-slide-up',
-  }" class="flex justify-center items-center" content-class="max-w-xl mx-4 p-4 bg-white rounded-lg space-y-2">
+  }" class="flex justify-center items-center"
+    content-class="max-w-full mx-4 bg-white rounded-lg space-y-2 overflow-hidden">
     <slot></slot>
   </VueFinalModal>
 </template>
