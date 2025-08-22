70b1f097ddc2d701020d3d113d6a5034e9ac9ab6
bino <bonoself@gmail.com>
2025-08-22 19:13:50 +0900

    .

--- server/.output/server/chunks/routes/api/health.mjs
+++
@@ -1,2 +0,0 @@
-import{a as o}from"../../nitro/nitro.mjs";import"node:process";import"cloudflare:workers";import"node:events";import"node:buffer";import"node:timers";import"node:async_hooks";const r=o(()=>"ok");export{r as default};
-//# sourceMappingURL=health.mjs.map
